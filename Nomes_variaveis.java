9a128d9c423db821637c0011f587cd3bb5fde6f1
Thiago Lahass <91209141+ThiagoLahass@users.noreply.github.com>
2023-01-12 18:15:32 -0300

    Add files via upload

---
+++ Nomes_variaveis.java
@@ -0,0 +1,21 @@
+public class Nomes_variaveis {
+
+    public static void main(String[] args){
+
+        // validos:
+        int num = 0;
+        int numInteiro = 128;
+        int num10 = 10;
+        int num_int = 1;
+        int NUM = 12;
+
+        // invalidos:
+        /*
+        int 
+         
+         */
+        
+        System.out.println("Hello, World!");
+        System.out.println(num);
+    }
+}
